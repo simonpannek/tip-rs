6719974ed93eb128b89c4e87580bc4a64d8cb2ad
Simon Pannek <simon.pannek@tum.de>
2023-02-26 13:52:27 +0100

    Add logout api point

---
+++ web-client/src/routes/api/logout/+server.js
@@ -0,0 +1,13 @@
+export function GET() {
+	// Set cookies and redirect client to main page
+	return new Response(null, {
+		headers: {
+			'Set-Cookie': [
+				'access_token=logout; Path=/; HttpOnly; SameSite=Strict; Expires=Thu, 01 Jan 1970 00:00:00 GMT',
+				'refresh_token=logout; Path=/; HttpOnly; SameSite=Strict; Expires=Thu, 01 Jan 1970 00:00:00 GMT'
+			],
+			Location: '/'
+		},
+		status: 302
+	});
+}
