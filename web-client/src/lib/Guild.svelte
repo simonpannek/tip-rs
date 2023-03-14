13c85fd2734b274275cbc3308e686a7176a5765f
Simon Pannek <simon.pannek@tum.de>
2023-03-14 08:03:32 +0100

    Design guild elements

--- web-client/src/lib/Guild.svelte
+++ web-client/src/lib/Guild.svelte
@@ -1,7 +1,23 @@
 <script>
 	export let guild;
+
+	function guildIcon(guild) {
+		return guild.icon && `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.webp`;
+	}
 </script>
 
-<li>
-	{JSON.stringify(guild)}
+<li id={guild.id}>
+	<div class="flex flex-col items-center">
+	<img
+		src={guildIcon(guild)}
+		alt={guild.name
+			.split(' ')
+			.map((word) => word[0])
+			.slice(0, 3)
+			.join('')
+			.toUpperCase()}
+	/>
+	<h2>{guild.name}</h2>
+	</div>
+	<div style:background={`center/cover url(${guildIcon(guild)})`} class="blur absolute" />
 </li>
